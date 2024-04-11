d80cd87e985d8a598f4b50208250254ab3e7dd20
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-04-11 09:03:44 -0400

    Fixed PayrollCalculator.java

--- workbook_1/PayrollCalculator.java
+++ workbook_1/PayrollCalculator.java
@@ -2,13 +2,19 @@ package workbook_1;
 import java.util.Scanner;
 
 public class PayrollCalculator {
-    public static void main(String[] args) {
-        // Initalize the scanner.
-        Scanner scanner = new Scanner(System.in);
+    // Initalize the scanner.
+    static Scanner scanner = new Scanner(System.in);
 
+    // Create the variables.
+    static String name;
+    static int hoursWorked;
+    static double payRate;
+    static String overtime;
+
+    public static void main(String[] args) {
         // Tell the user to input their name.
         System.out.println("Please input your name.");
-        String name = scanner.next();
+        String name = scanner.nextLine();
 
         // Tell the user to input their hours worked.
         System.out.println("Please input your hours worked.");
@@ -17,30 +23,39 @@ public class PayrollCalculator {
         // Tell the user to input their pay rate.
         System.out.println("Please input your pay rate.");
         double payRate = scanner.nextDouble();
+        scanner.nextLine();
 
         // Ask user if they worked overtime.
         System.out.println("Did you work overtime this week? (Yes/No)");
         String overtime = scanner.nextLine();
 
         if (overtime.equalsIgnoreCase("yes")) {
-            // Ask user how many hours did they work overtime.
-            System.out.println("How many hours did you work overtime?");
-            int overtimeHours = scanner.nextInt();
-
-            // Calculate the overtime payrate.
-            double overtimePayRate = payRate * 1.5;
-
-            // Calculate the total gross pay with the overtime pay + regular pay.
-            double grossPay = (overtimeHours + overtimePayRate) + (hoursWorked * payRate);
-
-            // Print out the information if user worked overtime.
-            System.out.println(name + "'s" + " Gross Pay: $" + grossPay);
+            didOvertime();
         } else if (overtime.equalsIgnoreCase("no")) {
-            // Calculate the gross pay.
-            double grossPay = hoursWorked * payRate;
-
-            // Print out the information if user didn't work overtime.
-            System.out.println(name + "'s" + " Gross Pay: $" + grossPay);
+            noOvertime();
         }
+    }
+
+    public static void didOvertime() {
+        // Ask user how many hours did they work overtime.
+        System.out.println("How many hours did you work overtime?");
+        int overtimeHours = scanner.nextInt();
+
+        // Calculate the overtime payrate.
+        double overtimePayRate = payRate * 1.5;
+
+        // Calculate the total gross pay with the overtime pay + regular pay.
+        double grossPay = (overtimeHours + overtimePayRate) + (hoursWorked * payRate);
+
+        // Print out the information if user worked overtime.
+        System.out.println(name + "'s" + " Gross Pay: $" + grossPay);
+    }
+
+    public static void noOvertime() {
+        // Calculate the gross pay.
+        double grossPay = hoursWorked * payRate;
+
+        // Print out the information if user didn't work overtime.
+        System.out.println(name + "'s" + " Gross Pay: $" + grossPay);
     }
 }
