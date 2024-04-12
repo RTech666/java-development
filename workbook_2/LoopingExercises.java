fee632b3aac4c8bcc190b6ab4e0900322f36a3ae
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-04-12 11:11:49 -0400

    Added comments to LoopingExercises.java

--- workbook_2/LoopingExercises.java
+++ workbook_2/LoopingExercises.java
@@ -2,14 +2,23 @@ package workbook_2;
 
 public class LoopingExercises {
     public static void main(String[] args) throws InterruptedException {
+        // Call the WhileLoop class.
         WhileLoop.main(args);
+
+        // Call the DoWhileLoop class.
         DoWhileLoop.main(args);
+
+        // Call the ForLoop class.
         ForLoop.main(args);
     }
 
+    // WhileLoop class.
     static class WhileLoop {
         public static void main(String[] args) {
+            // Create the variable.
             int count = 0;
+
+            // Print "I love Java" five times.
             while (count < 5) {
                 System.out.println("I love Java");
                 count++;
@@ -17,9 +26,13 @@ public class LoopingExercises {
         }
     }
 
+    // DoWhileLoop class.
     static class DoWhileLoop {
         public static void main(String[] args) {
+            // Create the variable.
             int count = 0;
+
+            // Print "I love Java" five times.
             do {
                 System.out.println("I love Java");
                 count++;
@@ -27,8 +40,10 @@ public class LoopingExercises {
         }
     }
 
+    // ForLoop class.
     static class ForLoop {
         public static void main(String[] args) throws InterruptedException {
+            // Count down from 10, once it hits 0, it prints "Launch!".
             for (int i = 10; i > 0; i--) {
                 System.out.println(i);
                 Thread.sleep(1000);
