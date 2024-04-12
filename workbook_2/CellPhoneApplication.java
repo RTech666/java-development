5d3779ed55beed0f7d5e5b3eff6421235d26bf8c
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-04-11 20:42:56 -0400

    Fixed files and did the second CellPhoneService.java exercise.

--- workbook_2/CellPhoneApplication.java
+++ workbook_2/CellPhoneApplication.java
@@ -1,5 +1,5 @@
 package workbook_2;
-import workbook_2.com.pluralsight.CellPhoneService;
+import workbook_2.com.pluralsight.CellPhone;
 import java.util.Scanner;
 
 public class CellPhoneApplication {
@@ -12,17 +12,27 @@ public class CellPhoneApplication {
     static String carrier;
     static String phoneNumber;
     static String owner;
-    static CellPhoneService CellPhone;
+    static String dial;
+    static CellPhone phone;
 
     public static void main(String[] args) {
-        // Call the enterInformation method.
+        // First enterInformation gets saved as cellPhone1.
         enterInformation();
-
-        // Call the parseInformation method.
         parseInformation();
+        CellPhone cellPhone1 = phone;
 
-        // Call the printInformation method.
-        printInformation();
+        // Second enterInformation gets saved as cellPhone2
+        enterInformation();
+        parseInformation();
+        CellPhone cellPhone2 = phone;
+
+        // Show the information for both phones.
+        display(cellPhone1);
+        display(cellPhone2);
+
+        // Dial the phones.
+        cellPhone1.dial(cellPhone2.getPhoneNumber());
+        cellPhone2.dial(cellPhone1.getPhoneNumber());
     }
 
     // Ask the user to input the information for the phone.
@@ -46,16 +56,17 @@ public class CellPhoneApplication {
 
     // Create the CellPhone opject from CallPhoneService class.
     public static void parseInformation() {
-        CellPhone = new CellPhoneService(serialNumber, model, carrier, phoneNumber, owner);
+        phone = new CellPhone(serialNumber, model, carrier, phoneNumber, owner);
     }
 
     // Print out the information provided.
-    public static void printInformation() {
+    public static void display(CellPhone phone) {
         System.out.println("\nPhone Information:");
-        System.out.println("Serial Number: " + CellPhone.getSerialNumber());
-        System.out.println("Phone Model: " + CellPhone.getModel());
-        System.out.println("Carrier: " + CellPhone.getCarrier());
-        System.out.println("Phone Number: " + CellPhone.getPhoneNumber());
-        System.out.println("Owner: " + CellPhone.getOwner());
+        System.out.println("Serial Number: " + phone.getSerialNumber());
+        System.out.println("Phone Model: " + phone.getModel());
+        System.out.println("Carrier: " + phone.getCarrier());
+        System.out.println("Phone Number: " + phone.getPhoneNumber());
+        System.out.println("Owner: " + phone.getOwner() + "\n");
     }
+
 }
--- workbook_2/com/pluralsight/CellPhone.java
+++ workbook_2/com/pluralsight/CellPhone.java
@@ -1,57 +1,61 @@
 package workbook_2.com.pluralsight;
 
-public class CellPhoneService {
+public class CellPhone {
     private int serialNumber = 0;
     private String model = "";
     private String carrier = "";
     private String phoneNumber = "";
     private String owner = "";
 
-    public CellPhoneService(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
         this.serialNumber = serialNumber;
         this.model = model;
         this.carrier = carrier;
         this.phoneNumber = phoneNumber;
         this.owner = owner;
-    }
-
-    public int getSerialNumber() {
-        return this.serialNumber;
     }
 
     public void setSerialNumber(int serialNumber) {
         this.serialNumber = serialNumber;
     }
 
-    public String getModel() {
-        return this.model;
+    public int getSerialNumber() {
+        return this.serialNumber;
     }
 
     public void setModel(String model) {
         this.model = model;
     }
 
-    public String getCarrier() {
-        return this.carrier;
+    public String getModel() {
+        return this.model;
     }
 
     public void setCarrier(String carrier) {
         this.carrier = carrier;
     }
 
-    public String getPhoneNumber() {
-        return this.phoneNumber;
+    public String getCarrier() {
+        return this.carrier;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getOwner() {
-        return this.owner;
+    public String getPhoneNumber() {
+        return this.phoneNumber;
     }
 
     public void setOwner(String owner) {
         this.owner = owner;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public void dial(String phoneNumber) {
+        System.out.println(owner + "'s phone is calling " + phoneNumber);
     }
 }
