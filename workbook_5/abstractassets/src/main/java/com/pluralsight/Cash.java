dfd16b134b78bab6759ebd572b87c964012049d6
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-04-26 11:56:57 -0400

    Completed final exercise.

---
+++ workbook_5/abstractassets/src/main/java/com/pluralsight/Cash.java
@@ -0,0 +1,8 @@
+package main.java.com.pluralsight;
+
+public class Cash extends Asset {
+    // Create the constructor.
+    public Cash(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+}
