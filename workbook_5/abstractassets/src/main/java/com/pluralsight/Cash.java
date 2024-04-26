f6ac885d1decd772ad148c93403d62e299e3dd99
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-04-26 13:01:34 -0400

    Fixed error in Cash.java.

--- workbook_5/abstractassets/src/main/java/com/pluralsight/Cash.java
+++ workbook_5/abstractassets/src/main/java/com/pluralsight/Cash.java
@@ -1,8 +1,14 @@
-package main.java.com.pluralsight;
+package com.pluralsight;
 
 public class Cash extends Asset {
     // Create the constructor.
     public Cash(String description, String dateAcquired, double originalCost) {
         super(description, dateAcquired, originalCost);
+    }
+
+    // Create the override getValue() method.
+    @Override
+    public double getValue() {
+        return getOriginalCost();
     }
 }
