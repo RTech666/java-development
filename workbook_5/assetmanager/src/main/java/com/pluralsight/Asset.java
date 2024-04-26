a190ef11c370b5c53e837e35553d68e150325f61
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-04-26 11:54:10 -0400

    Completed third exercise.

---
+++ workbook_5/assetmanager/src/main/java/com/pluralsight/Asset.java
@@ -0,0 +1,44 @@
+package com.pluralsight;
+
+class Asset {
+    // Create the variables, as private.
+    private String description;
+    private String dateAcquired;
+    private double originalCost;
+
+    // Create the contructor.
+    public Asset(String description, String dateAcquired, double originalCost) {
+        this.description = description;
+        this.dateAcquired = dateAcquired;
+        this.originalCost = originalCost;
+    }
+
+    // Create the getters and setters.
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateAcquired() {
+        return dateAcquired;
+    }
+
+    public void setDateAcquired(String dateAcquired) {
+        this.dateAcquired = dateAcquired;
+    }
+
+    public double getOriginalCost() {
+        return originalCost;
+    }
+
+    public void setOriginalCost(double originalCost) {
+        this.originalCost = originalCost;
+    }
+
+    public double getValue() {
+        return originalCost;
+    }
+}
