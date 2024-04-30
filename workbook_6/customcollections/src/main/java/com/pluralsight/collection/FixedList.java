5a3f2e3a79f9845911d75e22c9f998d351c7e7bb
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-04-30 10:31:51 -0400

    Added comments.

--- workbook_6/customcollections/src/main/java/com/pluralsight/collection/FixedList.java
+++ workbook_6/customcollections/src/main/java/com/pluralsight/collection/FixedList.java
@@ -3,14 +3,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FixedList<T> {
+    // Create the variables, as private and final.
     private final List<T> items;
     private final int maxSize;
 
+    // Create the constructor.
     public FixedList(int maxSize) {
         this.maxSize = maxSize;
         this.items = new ArrayList<>();
     }
 
+    // Create add method.
     public void add(T item) {
         if (items.size() < maxSize) {
             items.add(item);
@@ -19,6 +22,7 @@ public class FixedList<T> {
         }
     }
 
+    // Create getter.
     public List<T> getItems() {
         return items;
     }
