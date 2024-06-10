c72b724c87110ae7e40f9ab5263b8bb26d1786da
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-06-10 10:14:00 -0400

    Completed Exercise 2.

--- workbook_8/northwindproducts/src/main/java/com/pluralsight/Main.java
+++ workbook_8/northwindproducts/src/main/java/com/pluralsight/Main.java
@@ -7,31 +7,26 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "admin", "TC2%T@ajrGUhcB");
-            Statement statement = connection.createStatement()) {
+        // Connect to database.
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "TC2%T@ajrGUhcB");
+             Statement statement = connection.createStatement()) {
 
-            // Define query to select all products.
-            String selectQuery = "SELECT * FROM products";
-
-            // Execute query.
-            try (ResultSet allProducts = statement.executeQuery(selectQuery)) {
-                System.out.println("All Products:");
-                while (allProducts.next()) {
-                    int productId = allProducts.getInt("ProductID");
-                    String productName = allProducts.getString("ProductName");
-                    System.out.println("ID: " + productId + ", Name: " + productName);
-                }
-            }
-
-            // Define query to select all product names.
-            String queryProductNames = "SELECT ProductName FROM products";
+            // Define query to select product details.
+            String queryProductDetails = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
 
             // Execute query
-            try (ResultSet productNames = statement.executeQuery(queryProductNames)) {
-                System.out.println("\nProduct Names:");
-                while (productNames.next()) {
-                    String productName = productNames.getString("ProductName");
-                    System.out.println(productName);
+            try (ResultSet productDetails = statement.executeQuery(queryProductDetails)) {
+                while (productDetails.next()) {
+                    int productId = productDetails.getInt("ProductID");
+                    String productName = productDetails.getString("ProductName");
+                    double unitPrice = productDetails.getDouble("UnitPrice");
+                    int unitsInStock = productDetails.getInt("UnitsInStock");
+
+                    System.out.println("ID: " + productId);
+                    System.out.println("Name: " + productName);
+                    System.out.println(String.format("Price: %.2f", unitPrice));
+                    System.out.println("Stock: " + unitsInStock);
+                    System.out.println("------------------");
                 }
             }
 
