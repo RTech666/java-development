03c6b27e58b6252a58acc2204b0b8f41a5f46ec3
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-06-10 09:56:52 -0400

    Completed exercise 1.

---
+++ workbook_8/northwindproducts/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,42 @@
+package com.pluralsight;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "admin", "TC2%T@ajrGUhcB");
+             Statement statement = connection.createStatement()) {
+
+            // Define query to select all products.
+            String selectQuery = "SELECT * FROM products";
+
+            // Execute query.
+            try (ResultSet allProducts = statement.executeQuery(selectQuery)) {
+                System.out.println("All Products:");
+                while (allProducts.next()) {
+                    int productId = allProducts.getInt("ProductID");
+                    String productName = allProducts.getString("ProductName");
+                    System.out.println("ID: " + productId + ", Name: " + productName);
+                }
+            }
+
+            // Define query to select all product names.
+            String queryProductNames = "SELECT ProductName FROM products";
+
+            // Execute query
+            try (ResultSet productNames = statement.executeQuery(queryProductNames)) {
+                System.out.println("\nProduct Names:");
+                while (productNames.next()) {
+                    String productName = productNames.getString("ProductName");
+                    System.out.println(productName);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
